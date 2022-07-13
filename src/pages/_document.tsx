a50eb84a17edadd420078f20728514cd7c3ab4d0
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-07-13 15:57:32 -0300

    feat: added google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+import Analytics from '../components/Analytics';
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
         <Head>
+          <Analytics />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
@@ -15,6 +18,9 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="favicon.png" />
         </Head>
         <body>
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXXDTQT"></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
