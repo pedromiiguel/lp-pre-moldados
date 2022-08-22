efddca9e271b36d7e4990a224bbe7c644678f2fc
Pedro Miguel <pedromrap@gmail.com>
2022-08-22 20:22:00 -0300

    alteração na seção de produtos

--- src/components/Sections/SectionProducts/index.tsx
+++ src/components/Sections/SectionProducts/index.tsx
@@ -1,15 +1,82 @@
-import { Heading } from '@chakra-ui/react';
+import {
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Text,
+  useMediaQuery,
+} from '@chakra-ui/react';
 import React from 'react';
 import { Carousel } from '../../Carousel';
 import Container from '../../Container';
+import { products } from '../../Carousel/produts';
 
 const SectionProducts = () => {
+  const [isSmallThan768] = useMediaQuery('(max-width: 768px)');
+
   return (
-    <Container py={32} as="section">
+    <Container
+      py={16}
+      as="section"
+      borderTop="1px solid"
+      borderColor="gray.100"
+    >
       <Heading textAlign="center" mb={10}>
         Produtos
       </Heading>
-      <Carousel />
+      {isSmallThan768 ? (
+        <Grid templateColumns="1fr 1fr" gap={4}>
+          {products.map((product) => (
+            <Flex
+              key={`${product.name} ${product.description}`}
+              width="100%"
+              flexDirection="column"
+              textAlign="center"
+            >
+              <Image
+                src={product.image}
+                alt={product.name}
+                width="100%"
+                height="200px"
+                objectFit="contain"
+              />
+
+              <Heading color="gray.900" size="md" py={5}>
+                {product.name}
+              </Heading>
+
+              {!!product.price && !Array.isArray(product.price) && (
+                <>
+                  <Text color="gray.900" fontWeight="bold" mr={2} as="span">
+                    Preço:
+                  </Text>
+                  <Text color="gray.900">{product.price}</Text>
+                </>
+              )}
+
+              <Text color="gray.900" fontWeight="bold" mr={2} as="span">
+                Medida:
+              </Text>
+              <Text color="gray.900">{product.description}</Text>
+
+              {!!product.price && Array.isArray(product.price) && (
+                <Text color="gray.900" fontWeight="bold" mr={2}>
+                  Preços:
+                </Text>
+              )}
+              {!!product.price &&
+                Array.isArray(product.price) &&
+                product.price.map((price: string) => (
+                  <Text color="gray.900" key={price}>
+                    {price}
+                  </Text>
+                ))}
+            </Flex>
+          ))}
+        </Grid>
+      ) : (
+        <Carousel />
+      )}
     </Container>
   );
 };
