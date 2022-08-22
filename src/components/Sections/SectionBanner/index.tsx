0cad141b0e503bedc51278f86b6ad659d10d1a29
Pedro Miguel <pedromrap@gmail.com>
2022-08-22 20:17:22 -0300

    alteração na section banner

--- src/components/Sections/SectionBanner/index.tsx
+++ src/components/Sections/SectionBanner/index.tsx
@@ -1,51 +1,82 @@
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading, Icon } from '@chakra-ui/react';
+import Link from 'next/link';
 import React from 'react';
+import Container from '../../Container';
 import { Navbar } from '../../Navbar';
-
-const content = {
-  firstTitle: 'N° 1 EM BLOCOS E CANALETAS',
-  secondTitle: 'DE CONCRETO EM BRASÍLIA',
-};
+import content from './content';
 
 export const SectionBanner = () => {
   return (
     <Box
-    as="header"
-    width="100%"
-    height="100vh"
+      as="section"
+      width="100%"
+      height={['100%', '100%', '100vh', '100vh']}
       sx={{
         background:
-        'linear-gradient(#fffbfb92, #dad5d547), url(banner.jpg) no-repeat',
+          'linear-gradient(#fffbfb92, #dad5d547), url(banner.jpg) no-repeat',
         backgroundSize: 'cover',
         backgroundAttachment: 'fixed',
         backgroundPosition: 'center',
       }}
-      >
+    >
       <Navbar />
-      <Box width="100%" height="100%" bg="transparent">
+      <Container height="100%">
         <Flex
+          height="100%"
+          flexDirection="column"
           alignItems="center"
           justifyContent="center"
-          height="100%"
-          width="100%"
-          mx="auto"
         >
+          <Heading fontSize={['2xl', '3xl']} mt={8}>
+            O que você procura ?
+          </Heading>
           <Flex
-            flexDirection="column"
+            my={8}
+            align="center"
             justifyContent="center"
-            alignItems="center"
-            color="black"
-            px={[4, 8, 8, 8]}
+            gap={8}
+            flexDirection={['column', 'column', 'row']}
           >
-            <Heading fontSize={['3xl', '5xl']} lineHeight={['3xl', '5xl']}>
-              {content.firstTitle}
-            </Heading>
-            <Heading fontSize={['3xl', '5xl']} lineHeight={['3xl', '5xl']}>
-              {content.secondTitle}
-            </Heading>
+            {content.cards.map((service) => (
+              <Link href={service.href} passHref key={service.name}>
+                <Flex
+                  borderRadius="16px"
+                  align="center"
+                  justifyContent="center"
+                  flexDirection="column"
+                  bg="blue.900"
+                  color="gray.50"
+                  width="100%"
+                  height="200px"
+                  p={8}
+                  sx={{
+                    boxShadow: '0px 0px 12px rgba(0, 0, 0, 0.555)',
+                  }}
+                  transition="transform .3s"
+                  cursor="pointer"
+                  as="a"
+                  _hover={{
+                    filter: 'brightness(0.9)',
+                  }}
+                >
+                  <Icon
+                    mr="4"
+                    fontSize="48"
+                    _groupHover={{
+                      color: 'white',
+                    }}
+                    as={service.icon}
+                  />
+
+                  <Heading textAlign="center" size="md" my={8}>
+                    {service.name}
+                  </Heading>
+                </Flex>
+              </Link>
+            ))}
           </Flex>
         </Flex>
-      </Box>
+      </Container>
     </Box>
   );
 };
