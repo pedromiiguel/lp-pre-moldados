4600e1e98710cdd04594a507c839659670e0ce4a
Pedro Miguel <pedromrap@gmail.com>
2022-11-29 11:53:39 -0300

    adicionado google search console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="jbT2KCHcmHzljhLV8QHInIaMsvY0PKmwg-xvARmE2F0"
+        />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
