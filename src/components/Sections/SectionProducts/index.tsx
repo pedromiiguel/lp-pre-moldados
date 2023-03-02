2cbb1568822e42835dbb53d54228db3d8284fda0
Pedro Miguel <pedromrap@gmail.com>
2023-03-02 10:46:01 -0300

    style: remove border top

--- src/components/Sections/SectionProducts/index.tsx
+++ src/components/Sections/SectionProducts/index.tsx
@@ -18,8 +18,6 @@ const SectionProducts = () => {
     <Container
       py={16}
       as="section"
-      borderTop="1px solid"
-      borderColor="gray.100"
     >
       <Heading textAlign="center" mb={10}>
         Produtos
@@ -45,32 +43,10 @@ const SectionProducts = () => {
                 {product.name}
               </Heading>
 
-              {!!product.price && !Array.isArray(product.price) && (
-                <>
-                  <Text color="gray.900" fontWeight="bold" mr={2} as="span">
-                    Preço:
-                  </Text>
-                  <Text color="gray.900">{product.price}</Text>
-                </>
-              )}
-
               <Text color="gray.900" fontWeight="bold" mr={2} as="span">
                 Medida:
               </Text>
               <Text color="gray.900">{product.description}</Text>
-
-              {!!product.price && Array.isArray(product.price) && (
-                <Text color="gray.900" fontWeight="bold" mr={2}>
-                  Preços:
-                </Text>
-              )}
-              {!!product.price &&
-                Array.isArray(product.price) &&
-                product.price.map((price: string) => (
-                  <Text color="gray.900" key={price}>
-                    {price}
-                  </Text>
-                ))}
             </Flex>
           ))}
         </Grid>
