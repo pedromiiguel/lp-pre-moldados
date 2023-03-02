ff1e017804da3bc491a094329f090e3fd47410bb
Pedro Miguel <pedromrap@gmail.com>
2023-03-02 10:48:06 -0300

    feat: created new section with filter product filter

--- src/components/Sections/SectionBanner/index.tsx
+++ src/components/Sections/SectionBanner/index.tsx
@@ -1,82 +1,154 @@
-import { Box, Flex, Heading, Icon } from '@chakra-ui/react';
-import Link from 'next/link';
+import { Box, Button, Flex, Grid, Stack, Text } from '@chakra-ui/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import React from 'react';
+import Aside from '../../Aside';
 import Container from '../../Container';
 import { Navbar } from '../../Navbar';
-import content from './content';
+import { Navigation, Pagination } from 'swiper';
+
+import { products } from '../../Carousel/produts';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+type Categories = {
+  [key in string]: string;
+};
+
+const categories: Categories = {
+  bloco: 'Blocos',
+  canaleta: 'Canaletas',
+  muro: 'Muros e Casas Pré-moldadas',
+  manilha: 'Manilhas, Tubos e Calhas',
+  calcamento: 'Calçamento e Meio fio',
+  decoracao: 'Decoração',
+  suporte: 'Suportes para energia solar',
+};
 
 export const SectionBanner = () => {
-  return (
-    <Box
-      as="section"
-      width="100%"
-      height={['100%', '100%', '100vh', '100vh']}
-      sx={{
-        background:
-          'linear-gradient(#fffbfb92, #dad5d547), url(banner.jpg) no-repeat',
-        backgroundSize: 'cover',
-        backgroundAttachment: 'fixed',
-        backgroundPosition: 'center',
-      }}
-    >
-      <Navbar />
-      <Container height="100%">
-        <Flex
-          height="100%"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Heading fontSize={['2xl', '3xl']} mt={8}>
-            O que você procura ?
-          </Heading>
-          <Flex
-            my={8}
-            align="center"
-            justifyContent="center"
-            gap={8}
-            flexDirection={['column', 'column', 'row']}
-          >
-            {content.cards.map((service) => (
-              <Link href={service.href} passHref key={service.name}>
-                <Flex
-                  borderRadius="16px"
-                  align="center"
-                  justifyContent="center"
-                  flexDirection="column"
-                  bg="blue.900"
-                  color="gray.50"
-                  width="100%"
-                  height="200px"
-                  p={8}
-                  sx={{
-                    boxShadow: '0px 0px 12px rgba(0, 0, 0, 0.555)',
-                  }}
-                  transition="transform .3s"
-                  cursor="pointer"
-                  as="a"
-                  _hover={{
-                    filter: 'brightness(0.9)',
-                  }}
-                >
-                  <Icon
-                    mr="4"
-                    fontSize="48"
-                    _groupHover={{
-                      color: 'white',
-                    }}
-                    as={service.icon}
-                  />
+  const { query } = useRouter();
 
-                  <Heading textAlign="center" size="md" my={8}>
-                    {service.name}
-                  </Heading>
-                </Flex>
-              </Link>
-            ))}
-          </Flex>
+  const filterProducts = products.filter(
+    (product) => product.category === query.category
+  );
+
+  const data = !!filterProducts.length ? filterProducts : products.slice(1, 9);
+
+  return (
+    <>
+      <Navbar backgroundColor="transparent" isBorderBottom />
+      <Container height="100%" width="100%" mt={4}>
+        <Flex flexDirection={['column', 'column', 'column', 'row']}>
+          <Aside quantity={data.length} />
+          <Box flexDirection="column">
+            <Box
+              maxWidth={1000}
+              maxHeight={300}
+              width="100%"
+              height="100%"
+              overflow="hidden"
+            >
+              <Swiper
+                slidesPerView={1}
+                slidesPerGroup={1}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                loop={false}
+                loopFillGroupWithBlank={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <Image
+                    src="/home/muro.jpg"
+                    alt="Muro de concreto"
+                    width={1000}
+                    height={300}
+                    objectFit="cover"
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Image
+                    src="/home/blocos.jpg"
+                    alt="Muro de concreto"
+                    width={1000}
+                    height={300}
+                    objectFit="cover"
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Image
+                    src="/home/teste.jpg"
+                    alt="Muro de concreto"
+                    width={1000}
+                    height={300}
+                    objectFit="cover"
+                  />
+                </SwiperSlide>
+              </Swiper>
+            </Box>
+            <Grid
+              gridTemplateColumns={[
+                '1fr',
+                'repeat(2,1fr)',
+                'repeat(3,1fr)',
+                'repeat(4,1fr)',
+              ]}
+              columnGap="4"
+              rowGap={6}
+              flex="1"
+              padding={4}
+              justifyItems="center"
+            >
+              {data.map((product, index) => (
+                <Stack
+                  direction="column"
+                  align="flex-start"
+                  justify="space-between"
+                  spacing={2}
+                  key={index}
+                >
+                  <Image
+                    src={product.image}
+                    alt={product.name}
+                    width="225"
+                    height="225"
+                    objectFit="contain"
+                  />
+                  <Box>
+                    <Text fontSize="xs" textTransform="uppercase" opacity={0.7}>
+                      {categories[product.category]}
+                    </Text>
+                    <Text fontSize="sm" color="gray.600">
+                      {product.name}
+                    </Text>
+                  </Box>
+
+                  <Button
+                    as="a"
+                    aria-label="Open Whatsapp"
+                    target="_blank"
+                    href={`https://api.whatsapp.com/send?phone=5561994642112`}
+                    textTransform="uppercase"
+                    rounded="md"
+                    background="green.600"
+                    color="white"
+                    _hover={{
+                      background: 'green.700',
+                    }}
+                    _active={{
+                      background: 'green.700',
+                    }}
+                  >
+                    Solicitar orçamento
+                  </Button>
+                </Stack>
+              ))}
+            </Grid>
+          </Box>
         </Flex>
       </Container>
-    </Box>
+    </>
   );
 };
